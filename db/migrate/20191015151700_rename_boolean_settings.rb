bab8109a04373d80cb6cd71974352d97731705f1
Lobster <lobster@riseup.net>
2019-10-22 13:09:07 +0200

    Fix Rails 5.2 migrations

--- db/migrate/20191015151700_rename_boolean_settings.rb
+++ db/migrate/20191015151700_rename_boolean_settings.rb
@@ -1,4 +1,4 @@
-class RenameBooleanSettings < ActiveRecord::Migration
+class RenameBooleanSettings < ActiveRecord::Migration[5.2]
   def change
     rename_column :projects, :zulip_subject_issue,   :zulip_issue_updates
     rename_column :projects, :zulip_subject_version, :zulip_version_updates
--- db/migrate/20191018212300_remove_boolean_settings.rb
+++ db/migrate/20191018212300_remove_boolean_settings.rb
@@ -1,4 +1,4 @@
-class RemoveBooleanSettings < ActiveRecord::Migration
+class RemoveBooleanSettings < ActiveRecord::Migration[5.2]
   def change
     remove_column :projects, :zulip_private_messages, :boolean
     remove_column :projects, :zulip_issue_updates,    :boolean
