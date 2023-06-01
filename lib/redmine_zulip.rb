e631b94fc275723f104f7237ba57d6c1316b562b
Wurzelmann <Wurzelmann@users.noreply.github.com>
2023-06-01 14:59:12 +0200

    Update redmine_zulip.rb
    fixes Redmine 5 compatibility

--- lib/redmine_zulip.rb
+++ lib/redmine_zulip.rb
@@ -2,8 +2,6 @@ module RedmineZulip
   VERSION = "3.1.2"
 end
 
-Rails.configuration.to_prepare do
-  Issue.send(:include, RedmineZulip::IssuePatch)
-  Project.send(:include, RedmineZulip::ProjectPatch)
-  ProjectsController.send(:helper, RedmineZulip::ProjectSettingsTabs)
-end
+Issue.send(:include, RedmineZulip::IssuePatch)
+Project.send(:include, RedmineZulip::ProjectPatch)
+ProjectsController.send(:helper, RedmineZulip::ProjectSettingsTabs)
