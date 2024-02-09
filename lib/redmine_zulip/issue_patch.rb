13c1c932dd67a2f23d7a05d8c7156b0e705f681b
Wurzelmann <Wurzelmann@users.noreply.github.com>
2024-02-09 18:26:51 +0100

    Update issue_patch.rb
    Ruby 3 compatibility, works with Ruby >=2.7

--- lib/redmine_zulip/issue_patch.rb
+++ lib/redmine_zulip/issue_patch.rb
@@ -112,7 +112,7 @@ module RedmineZulip
     def notify_assignment
       locale = assigned_to.language.present? ?
                  assigned_to.language : Setting.default_language
-      message = I18n.t("zulip_notify_assignment", {
+      message = I18n.t("zulip_notify_assignment", **{
         locale: locale,
         user: User.current.name,
         id: id,
@@ -144,7 +144,7 @@ module RedmineZulip
       )
       locale = previous_assigned_to.language.present? ?
                  previous_assigned_to.language : Setting.default_language
-      message = I18n.t("zulip_notify_unassignment", {
+      message = I18n.t("zulip_notify_unassignment", **{
         user: User.current.name,
         id: id,
         url: url,
@@ -162,7 +162,7 @@ module RedmineZulip
     def notify_assigned_to_issue_updated
       locale = assigned_to.language.present? ?
                  assigned_to.language : Setting.default_language
-      message = I18n.t("zulip_notify_updated", {
+      message = I18n.t("zulip_notify_updated", **{
         user: User.current.name,
         id: id,
         url: url,
@@ -223,7 +223,7 @@ module RedmineZulip
     def notify_assigned_to_issue_destroyed
       locale = assigned_to.language.present? ?
                  assigned_to.language : Setting.default_language
-      message = I18n.t("zulip_notify_destroyed", {
+      message = I18n.t("zulip_notify_destroyed", **{
         user: User.current.name,
         id: id,
         project: project.name,
@@ -239,7 +239,7 @@ module RedmineZulip
 
     def init_issue_subject
       locale = Setting.default_language
-      message = I18n.t("zulip_init_issue_subject", {
+      message = I18n.t("zulip_init_issue_subject", **{
         locale: locale,
         user: User.current.name,
         id: id,
@@ -270,7 +270,7 @@ module RedmineZulip
 
     def update_issue_subject
       locale = Setting.default_language
-      message = I18n.t("zulip_notify_updated", {
+      message = I18n.t("zulip_notify_updated", **{
         locale: locale,
         user: User.current.name,
         id: id,
@@ -341,7 +341,7 @@ module RedmineZulip
     end
 
     def update_issue_subject_destroyed
-      message = I18n.t("zulip_notify_destroyed", {
+      message = I18n.t("zulip_notify_destroyed", **{
         locale: Setting.default_language,
         user: User.current.name,
         id: id,
@@ -357,7 +357,7 @@ module RedmineZulip
     end
 
     def update_version_subject_added
-      message = I18n.t("zulip_update_version_subject_added", {
+      message = I18n.t("zulip_update_version_subject_added", **{
         locale: Setting.default_language,
         user: User.current.name,
         id: id,
@@ -378,7 +378,7 @@ module RedmineZulip
       previous_fixed_version = Version.find(
         previous_changes["fixed_version_id"].first
       )
-      message = I18n.t("zulip_update_version_subject_removed", {
+      message = I18n.t("zulip_update_version_subject_removed", **{
         locale: Setting.default_language,
         user: User.current.name,
         id: id,
@@ -397,7 +397,7 @@ module RedmineZulip
 
     def update_version_subject_status
       previous_status_id = previous_changes["status_id"].first
-      message = I18n.t("zulip_update_version_subject_status", {
+      message = I18n.t("zulip_update_version_subject_status", **{
         locale: Setting.default_language,
         user: User.current.name,
         id: id,
@@ -416,7 +416,7 @@ module RedmineZulip
     end
 
     def update_version_subject_destroyed
-      message = I18n.t("zulip_notify_destroyed", {
+      message = I18n.t("zulip_notify_destroyed", **{
         locale: Setting.default_language,
         user: User.current.name,
         id: id,
